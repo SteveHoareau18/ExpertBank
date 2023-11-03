ebb105b592883ecbbbcbd0a779ee231f8a2e9758
Steve HOAREAU <shoareau.cda@FORMATION.LOCAL>
2023-11-03 12:38:43 +0400

    User class & jakarda dependencies

---
+++ src/main/java/net/expertbank/model/User.java
@@ -0,0 +1,68 @@
+package net.expertbank.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "UserBank")
+public class User implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String name,firstName,email,password,salt;
+	private int phoneNumber;
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", firstName=" + firstName + ", email=" + email + ", password="
+				+ password + ", salt=" + salt + ", phoneNumber=" + phoneNumber + "]";
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSalt() {
+		return salt;
+	}
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public long getId() {
+		return id;
+	}
+}
