bd30ae8febb526c16bb3e7eccc10a0a00fbea95a
maxime clain <maximeclaind@gmail.com>
2023-11-06 12:10:48 +0100

    local maxime

--- src/main/java/net/expertbank/controller/AdminController.java
+++ src/main/java/net/expertbank/controller/AdminController.java
@@ -1,15 +1,21 @@
 package net.expertbank.controller;
 
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Response;
 import net.expertbank.model.Administrator;
+import net.expertbank.model.Client;
 import net.expertbank.model.Operator;
 
 @Path("/admin")
+@RequestScoped
+@Transactional
 public class AdminController {
 	
 	@PersistenceContext
@@ -21,6 +27,7 @@ public class AdminController {
 	public Response generateDefault() {	
 		Administrator administrator = new Administrator();
 		administrator.setActive(true).setEmail("steve.hoareau1@gmail.com").setFirstName("Steve").setName("Hoareau").setPhoneNumber("0692010203").setPassword("steve1234").setSalt("");
+		em.persist(administrator);
 		return Response.ok(administrator).build();
 	}
 	
@@ -31,5 +38,15 @@ public class AdminController {
 		em.persist(operator);
 		
 		return Response.ok(operator).build();
+	}
+	
+	@PUT
+	@Path("/operator/update")
+	@Produces("application/json")
+	public Response operatorUpdate(Operator operator) {
+		
+		em.merge(operator);
+		
+		return Response.ok(operator).status(202).build();
 	}
 }
--- src/main/java/net/expertbank/controller/OperatorController.java
+++ src/main/java/net/expertbank/controller/OperatorController.java
@@ -1,14 +1,21 @@
 package net.expertbank.controller;
 
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Response;
+import net.expertbank.model.AccountBank;
 import net.expertbank.model.Client;
+import net.expertbank.services.IbanService;
 
 @Path("/operator")
+@RequestScoped
+@Transactional
 public class OperatorController {
 	
 	@PersistenceContext
@@ -17,9 +24,21 @@ public class OperatorController {
 	@POST
 	@Path("/client/create")
 	@Produces("application/json")
-	public Response operatorCreate(Client client) {
+	public Response clientCreate(Client client) {
 		em.persist(client);
+		AccountBank accountBank = new AccountBank();
+		accountBank.setIBAN(IbanService.generateIBAN()).setUserAccount(client);
+        em.persist(accountBank);
+        return Response.ok(accountBank).build();
+    }
+	
+	@PUT
+	@Path("/client/update")
+	@Produces("application/json")
+	public Response clientUpdate(Client client) {
 		
-		return Response.ok(client).build();
+		em.merge(client);
+		
+		return Response.ok(client).status(202).build();
 	}
 }
